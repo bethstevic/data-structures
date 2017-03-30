3b88b13a36e5c4e3f0c39e479f57a8d28cbf012b
YOUR NAME <bethstevic89@gmail.com>
2017-03-29 20:13:25 -0700

    Completed queue and stack on functional, started stack on functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,61 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+
+  extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+  var extend = function(to, from) {
+    for(var key in from) {
+      to[key] = from[key];
+    }
+  }
+
+  var stackMethods = {};
+
+  var counter = 0;
+
+  stackMethods.push = function(val) {
+    this.counter = val;
+    counter++;
+  }
+
+  stackMethods.pop = function() {
+    if(counter > 0) {
+      counter--;
+    }
+    return this.counter;
+  }
+
+  stackMethods.size = function() {
+    return counter;
+  }
+
+  stackMethods.result = function(){
+    return this.
+  }
+
+}
 
 
+//create stack function
+//set empty result object
+//set parameters passed in as the result objects values?
+//pass result object and helper object into extend function
+//return result object
+
+//create extend function that accepts two objects
+//set for in to loop through second object
+//copy second object into first
+
+//set empty helper object
+
+//set .push key w/function in helper object
+
+//set .pop key w/function in helper object
+
+//set .size key w/function in helper object
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var index = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
+    if (index > 0) {
+      index--;
+    }
+    var temp;
+    temp = storage[0];
+    delete storage[0];
+    for(var key in storage) {
+      storage[key - 1] = storage[key];
+    }
+    delete storage[someInstance.size() - 1];
+    return temp;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
