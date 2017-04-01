366e7538cd079f4b372a6158160760c6ae8e5dd8
YOUR NAME <bethstevic89@gmail.com>
2017-03-31 18:35:04 -0700

    Completed linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if(list.head !== null) {
+      var current = new Node(value);
+      list.tail.next = current;
+      list.tail = current;
+    }
+
+    if(list.head === null) {
+      list.head = new Node(value);
+      list.tail = list.head;
+    }
+
   };
 
   list.removeHead = function() {
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
   };
 
   list.contains = function(target) {
+  //start at head see if it contains value
+  //if not, go to node.next and see if contains value
+  //if not ect
+    var current = list.head;
+    while(current !== list.tail) {
+      if(current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+
+    if(list.tail.value !== target) {
+      return false;
+    } else {
+      return true;
+    }
   };
 
   return list;
