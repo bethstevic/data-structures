3d7f7bf53023d7bfac44ae0978c478dd8ba4835c
YOUR NAME <bethstevic89@gmail.com>
2017-04-01 16:41:58 -0700

    Finished hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,46 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  if(!bucket){
+  bucket = [];
+  bucket.push([k, v]);
+  this._storage[index] = bucket;
+  }else{
+    for(var i = 0; i < bucket.length; i++){
+      if(bucket[i][0] === k){
+        bucket[i][1] = v;
+      }else{
+        bucket.push([k, v]);
+      }
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
+  var result = undefined;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  if (!bucket){
+    result = 'Not Found';
+  }else {
+    for (var i = 0; i < bucket.length; i++){
+      if(bucket[i][0] === k){
+        result = bucket[i][1];
+      }
+    }
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  for (var i = 0; i < bucket.length; i++){
+    if(bucket[i][0] === k){
+      bucket.splice(i, 1);
+    }
+  }
 };
 
 
